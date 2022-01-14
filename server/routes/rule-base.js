2e9797e4b7e458bebc5d442194476ebc990c0bec
duck <trinhduc2712@gmail.com>
2022-01-14 15:27:01 +0700

    Save new case

--- server/routes/rule-base.js
+++ server/routes/rule-base.js
@@ -18,7 +18,7 @@ const lineToObject = (line) => {
   };
 };
 
-const findByAgeAndGender = (age, gender) => physicalCondition.find((item) => item.age == age && item.gender == gender);
+const findByAgeAndGender = (physicalCondition, age, gender) => physicalCondition.find((item) => item.age == age && item.gender == gender);
 
 const findByWeightAndHeight = (physical, weight, height) => {
   let weightCondition = null;
@@ -40,35 +40,43 @@ const findByWeightAndHeight = (physical, weight, height) => {
   }
 }
 
-// File
-const physicalFile = fs.readFileSync(path.resolve(__dirname, "../data/rule.csv"));
-const physicalCondition = physicalFile
-  .toString()
-  .split(/\r?\n/)
-  .map((item) => lineToObject(item));
-
 // API
 router.post("/", (req, res) => {
   try {
+    // File
+    const physicalFile = fs.readFileSync(path.resolve(__dirname, "../data/rule.csv"), { encoding: "utf-8" });
+    let physicalCondition = physicalFile
+      .toString()
+      .split(/\r?\n/)
+      .map((item) => lineToObject(item));
+    physicalCondition = physicalCondition.slice(1, -1);
+
     const { age, gender, weight, height } = req.body;
     if (!age || !gender || !weight || !height)
       return res.status(400).json({ code: 400, message: "Invalid input" });
     
     // find by age and gender
     let physical = null;
-    if (age <= 24) physical = findByAgeAndGender(age, gender);
-    else if (age <= 30) physical = findByAgeAndGender(30, gender);
-    else if (age <= 36) physical = findByAgeAndGender(36, gender);
-    else if (age <= 42) physical = findByAgeAndGender(42, gender);
-    else if (age <= 48) physical = findByAgeAndGender(48, gender);
-    else if (age <= 54) physical = findByAgeAndGender(54, gender);
-    else physical = findByAgeAndGender(60, gender);
+    if (age <= 24) physical = findByAgeAndGender(physicalCondition, age, gender);
+    else if (age <= 30) physical = findByAgeAndGender(physicalCondition, 30, gender);
+    else if (age <= 36) physical = findByAgeAndGender(physicalCondition, 36, gender);
+    else if (age <= 42) physical = findByAgeAndGender(physicalCondition, 42, gender);
+    else if (age <= 48) physical = findByAgeAndGender(physicalCondition, 48, gender);
+    else if (age <= 54) physical = findByAgeAndGender(physicalCondition, 54, gender);
+    else physical = findByAgeAndGender(physicalCondition, 60, gender);
   
     // get physical condition
-    const result = findByWeightAndHeight(physical, weight, height);
+    let result = findByWeightAndHeight(physical, weight, height);
+    result.case = {
+      age: age,
+      gender: gender,
+      height: height,
+      weight: weight,
+    }
   
     return res.json(result);
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ code: 500, message: error.message })
   }
 });
